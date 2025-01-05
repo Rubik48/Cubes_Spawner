96441007d51791fb9bc636289a664bb56d6d44de
Roman Strakhov <43264457+Rubik48@users.noreply.github.com>
2025-01-04 20:07:41 -0500

    1

---
+++ Cube.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using Random = UnityEngine.Random;
+
+[RequireComponent(typeof(Renderer), typeof(Rigidbody))]
+public class Cube : MonoBehaviour
+{
+    [SerializeField] private float _minTimeLife = 2f;
+    [SerializeField] private float _maxTimeLife = 5f;
+    
+    private Renderer _renderer;
+    private Coroutine _countTimeLife;
+    
+    private float _timeLife;
+    private float _timeDelay = 1f;
+    private float _seconds = 0f;
+    private bool _wasChanged = false;
+    
+    public event Action<Cube> LifeTimeFinished;
+
+    private void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+        
+        _timeLife = Random.Range(_minTimeLife, _maxTimeLife);
+    }
+    
+    private void OnEnable()
+    {
+        _wasChanged = false;
+        _seconds = 0;
+        _timeLife = Random.Range(_minTimeLife, _maxTimeLife);
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Platform>() && _wasChanged == false)
+        {
+            _wasChanged = true;
+            
+            ChangeColor();
+
+            _countTimeLife = StartCoroutine(RunLifeTime());
+        }
+    }
+
+    private void OnDisable()
+    {
+        if(_countTimeLife != null)
+            StopCoroutine(_countTimeLife);
+    }
+
+    private void ChangeColor()
+    {
+        _renderer.material.color = Random.ColorHSV();
+    }
+    
+    private IEnumerator RunLifeTime()
+    {
+        while (_seconds <= _timeLife)
+        {
+            yield return new WaitForSeconds(_timeDelay);
+
+            _seconds++;
+        }
+        
+        LifeTimeFinished?.Invoke(this);
+    }
+}
---
+++ Spawner.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Pool;
+using Random = UnityEngine.Random;
+
+public class Spawner : MonoBehaviour
+{
+    [Header("Pool Settings")]
+    [SerializeField] private Cube _cubePrefab;
+    [SerializeField] private int _defaultCapacity = 5;
+    [SerializeField] private int _maxPoolSize = 5;
+
+    [Header("Spawn Settings")] 
+    [SerializeField] private float _spawnInterval = 1f;
+    [SerializeField] private float _spawnHeight = 10f;
+    [SerializeField] private float _spawnAreaWidth = 10f;
+
+    private IObjectPool<Cube> _cubePool;
+    private Cube _cube;
+    
+    private int _activeCubes = 0;
+
+    private void Awake()
+    {
+        _cubePool = new ObjectPool<Cube>(
+            createFunc: () => Instantiate(_cubePrefab),
+            actionOnGet: cube => cube.gameObject.SetActive(true),
+            actionOnRelease: cube => cube.gameObject.SetActive(false),
+            actionOnDestroy: cube => Destroy(cube.gameObject),
+            collectionCheck: true,
+            defaultCapacity: _defaultCapacity,
+            maxSize: _maxPoolSize
+        );
+    }
+
+    private void Start()
+    {
+        StartCoroutine(SpawnCubes());
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.Log(_cubePool.CountInactive);
+    }
+
+    private void SpawnCube()
+    {
+        _cube = _cubePool.Get();
+        
+        _activeCubes++;
+
+        _cube.LifeTimeFinished += ReleaseCube;
+        
+        _cube.transform.position = new Vector3(Random.Range(-_spawnAreaWidth/2, _spawnAreaWidth/2), _spawnHeight, Random.Range(-_spawnAreaWidth/2, _spawnAreaWidth/2));
+    }
+
+    private void ReleaseCube(Cube cube)
+    {
+        cube.LifeTimeFinished -= ReleaseCube;
+        
+        _activeCubes--;
+        
+        _cubePool.Release(cube);
+    }
+
+    private IEnumerator SpawnCubes()
+    {
+        while (true)
+        {
+            if (_activeCubes < _maxPoolSize)
+                SpawnCube();
+            
+            yield return new WaitForSeconds(_spawnInterval);
+        }
+    }
+}
